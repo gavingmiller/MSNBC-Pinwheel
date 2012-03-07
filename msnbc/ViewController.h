211836b4e4708a587f07e7d9d7256ef78a4043ea
Gavin Miller <gavingmiller@gmail.com>
2012-03-06 19:48:24 -0700

    Initial Commit

---
+++ msnbc/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  msnbc
+//
+//  Created by Gavin Miller on 12-03-06.
+//  Copyright (c) 2012 RandomType. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ msnbc/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  msnbc
+//
+//  Created by Gavin Miller on 12-03-06.
+//  Copyright (c) 2012 RandomType. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
